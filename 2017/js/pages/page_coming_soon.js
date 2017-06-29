152df6441c22292e865d0e43fe21988eb47ac18a
nababora <nababora@naver.com>
2017-06-29 10:45:19 +0900

    Update page_coming_soon.js

--- 2017/js/pages/page_coming_soon.js
+++ 2017/js/pages/page_coming_soon.js
@@ -2,7 +2,7 @@ var PageComingSoon = function () {
     return {
       //Coming Soon
       initPageComingSoon: function () {
-			var newYear = new Date('2017-06-30T09:00:00Z');
+			var newYear = new Date('2017-06-30T00:00:00Z');
 			//newYear = new Date(newYear + 1, 1 - 1, 1);
 			console.log(newYear);
 			$('#defaultCountdown').countdown({until: newYear})
