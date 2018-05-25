fd41b1e9f7c8d5d85441e8b95c66aa75eecdc7bc
JeongOhKyea <04151220@naver.com>
2018-05-25 14:32:12 +0900

    Fix Start Time

--- 2018/js/pages/page_coming_soon.js
+++ 2018/js/pages/page_coming_soon.js
@@ -2,7 +2,7 @@ var PageComingSoon = function () {
     return {
       //Coming Soon
       initPageComingSoon: function () {
-			var newYear = new Date('2018-09-14T10:00:00Z');
+			var newYear = new Date('2018-09-14T01:00:00Z');
 			//newYear = new Date(newYear + 1, 1 - 1, 1);
 			console.log(newYear);
 			$('#defaultCountdown').countdown({until: newYear})
