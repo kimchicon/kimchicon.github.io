646fefb3d5437a785a8d86eb9f1655a1cb96050b
JeongOhKyea <04151220@naver.com>
2018-05-25 12:59:01 +0900

    2018 Simple Start

---
+++ 2018/js/pages/page_coming_soon.js
@@ -0,0 +1,11 @@
+var PageComingSoon = function () {
+    return {
+      //Coming Soon
+      initPageComingSoon: function () {
+			var newYear = new Date('2018-09-14T10:00:00Z');
+			//newYear = new Date(newYear + 1, 1 - 1, 1);
+			console.log(newYear);
+			$('#defaultCountdown').countdown({until: newYear})
+        }
+    };
+}();
